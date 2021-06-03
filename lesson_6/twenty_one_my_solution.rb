b7b853acbe529613edbf5fce51da5c2f858097e9
Clare MacAdie <clare@macadie.co.uk>
2021-06-03 23:24:30 +0000

    21 game with total points displayed.

--- lesson_6/twenty_one_my_solution.rb
+++ lesson_6/twenty_one_my_solution.rb
@@ -21,6 +21,14 @@ def prompt(message)
   puts("=> #{message}")
 end
 
+def pronoun(participant)
+  if participant == 'player'
+    'You'
+  elsif participant == 'dealer'
+    'I'
+  end
+end
+
 def initialize_deck!
   result = CARD_SUITS.each_with_object({}) do |suit, hsh|
     CARD_RANKS.each do |value|
@@ -66,7 +74,16 @@ end
 def declare_initial_dealer_card_values(cards)
   dealer_cards = card_locations(cards, 'dealer')
   first_dealer_card = dealer_cards[0].split('_')[0]
-  prompt("Dealer has: #{first_dealer_card} and unknown card.")
+  prompt("#{pronoun('dealer')} have: #{first_dealer_card} and unknown card.")
+end
+
+def declare_prior_dealer_card_values(cards)
+  dealer_hand = card_locations(cards, 'dealer')
+  dealer_hand_without_suits = dealer_hand.each_with_object([]) do |card, arr|
+    arr << card.split('_')[0]
+  end
+  prompt("I have: #{joinor(dealer_hand_without_suits)}, "\
+    "for a total of #{hand_value(cards, 'dealer')}.")
 end
 
 def joinor(arr, delimiter=', ', word='and')
@@ -80,7 +97,25 @@ def joinor(arr, delimiter=', ', word='and')
   end
 end
 
+def declare_initial_player_card_values(cards)
+  hand = card_locations(cards, 'player')
+  hand_without_suits = hand.each_with_object([]) do |card, arr|
+    arr << card.split('_')[0]
+  end
+  prompt("#{pronoun('player')} have: #{joinor(hand_without_suits)}, "\
+    "for a total of #{hand_value(cards, 'player')}.")
+end
+
 def declare_participant_card_values(cards, participant)
+  hand = card_locations(cards, participant)
+  hand_without_suits = hand.each_with_object([]) do |card, arr|
+    arr << card.split('_')[0]
+  end
+  prompt("#{pronoun(participant)} now have: #{joinor(hand_without_suits)}, "\
+    "for a total of #{hand_value(cards, participant)}.")
+end
+
+def declare_final_participant_card_values(cards, participant)
   hand = card_locations(cards, participant)
   hand_without_suits = hand.each_with_object([]) do |card, arr|
     arr << card.split('_')[0]
@@ -92,7 +127,6 @@ end
 def ask_player_hit_or_stay
   loop do
     prompt(MESSAGES['ask_hit_or_stay'])
-    prompt(MESSAGES['instruction_for_hit_or_stay'])
     answer = gets.chomp
     return answer if VALID_INPUTS.include?(answer)
     prompt(MESSAGES['invalid_choice'])
@@ -104,23 +138,35 @@ def busted?(cards, participant)
 end
 
 def player_turn!(cards)
+  declare_initial_dealer_card_values(cards)
+  declare_initial_player_card_values(cards)
   loop do
-    declare_initial_dealer_card_values(cards)
-    declare_participant_card_values(cards, 'player')
     hit_or_stay = ask_player_hit_or_stay
     if hit_or_stay == 'h'
       prompt(MESSAGES['player_chose_hit'])
       deal_card!(cards, 'player')
+      declare_participant_card_values(cards, 'player')
     elsif hit_or_stay == 's'
-      prompt(MESSAGES['player_chose_stay'])
+      prompt("You stayed at #{hand_value(cards, 'player')}")
     end
     return if hit_or_stay == 's' || busted?(cards, 'player')
   end
 end
 
 def dealer_turn!(cards)
+  prompt(MESSAGES['dealer_turn'])
+  declare_prior_dealer_card_values(cards)
   loop do
-    hand_value(cards, 'dealer') < 17 ? deal_card!(cards, 'dealer') : return
+    if hand_value(cards, 'dealer') < 17
+      deal_card!(cards, 'dealer')
+      prompt(MESSAGES['dealer_hits'])
+      declare_participant_card_values(cards, 'dealer')
+    elsif busted?(cards, 'dealer')
+      return
+    else
+      prompt("Dealer stays at #{hand_value(cards, 'dealer')}")
+      return
+    end
   end
 end
 
@@ -130,15 +176,28 @@ def declare_winner(cards)
   elsif busted?(cards, 'dealer')
     prompt(MESSAGES['dealer_bust'])
   elsif hand_value(cards, 'player') > hand_value(cards, 'dealer')
+    prompt(MESSAGES['spacer_='])
+    declare_final_participant_card_values(cards, 'dealer')
+    declare_final_participant_card_values(cards, 'player')
+    prompt(MESSAGES['spacer_='])
     prompt(MESSAGES['player_win'])
-  else
+  elsif hand_value(cards, 'dealer') > hand_value(cards, 'player')
+    prompt(MESSAGES['spacer_='])
+    declare_final_participant_card_values(cards, 'dealer')
+    declare_final_participant_card_values(cards, 'player')
+    prompt(MESSAGES['spacer_='])
     prompt(MESSAGES['dealer_win'])
+  else
+    declare_final_participant_card_values(cards, 'dealer')
+    declare_final_participant_card_values(cards, 'player')
+    prompt(MESSAGES['tie'])
   end
 end
 
 def another_game?
   answer = ''
   loop do
+    prompt(MESSAGES['spacer_-'])
     prompt(MESSAGES['ask_another_game?'])
     answer = gets.chomp
     break if VALID_ANSWERS.include?(answer)
@@ -154,10 +213,8 @@ loop do
   cards = initialize_deck!
   deal_initial_cards!(cards)
   player_turn!(cards)
-  declare_participant_card_values(cards, 'player')
   if !busted?(cards, 'player')
     dealer_turn!(cards)
-    declare_participant_card_values(cards, 'dealer')
   end
 
   declare_winner(cards)
